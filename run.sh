23ed1b43175e6404e47ed1c6b3f1f07654ee8dc2
kawasemi <a-tomohiro@sumilab.org>
2017-06-18 16:04:11 +0900

    change wercker.yml

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t slim
+
+# Add contents and draft changes to git.
+git add content/
+
+# Go To Public(docs) folder
+cd docs
+
+# Add site changes to git.
+git add .
+
+# Commit changes.引数があれば第一引数をcommit messageに設定.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
